9ce3ec424d58ec1ec48c3f1ac22f52e46634c409
Abhiek187 <achaudhuri2011@yahoo.com>
2018-05-17 15:44:38 -0400

    style: Replace functions w/ arrow functions

--- js/app.js
+++ js/app.js
@@ -3,6 +3,21 @@
  */
 let cards = document.getElementsByClassName('card');
 
+// Shuffle function from http://stackoverflow.com/a/2450976
+const shuffle = (array) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -23,21 +38,6 @@ for (let card of cards) {
 
 deck.appendChild(fragment);
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -54,7 +54,7 @@ function shuffle(array) {
  * another function that you call from this one)
  */
 const restart = document.querySelector('.restart');
-restart.addEventListener('click', function() {
+restart.addEventListener('click', () => {
 	window.location.reload();
 });
 
@@ -68,7 +68,7 @@ const timer = document.querySelector('.timer');
 
 document.addEventListener('DOMContentLoaded', function startTimer() {
 	const start = new Date().getTime(); // time in milliseconds
-	const time = setInterval(function() {
+	const time = setInterval(() => {
 		if (cardPairs === 8)
 			clearInterval(time);
 
@@ -84,58 +84,12 @@ document.addEventListener('DOMContentLoaded', function startTimer() {
 	}, 0);
 });
 
-deck.addEventListener('click', function(event) {
-	if (canFlip) {
-		const card = event.target;
-
-		if (card.className === 'card')
-			flipCard(card);
-
-		if (openCards.length === 2)
-			checkCards(openCards);
-	}
-});
-
-function flipCard(card) {
+const flipCard = (card) => {
 	card.className += ' open show';
 	openCards.push(card);
 }
 
-function checkCards(cards) {
-	canFlip = false; // disable flipping cards while checking
-
-	setTimeout(function() {
-		if (cards[0].firstElementChild.className === cards[1].firstElementChild.className) {
-			cards[0].className = 'card match';
-			cards[1].className = 'card match';
-			cardPairs++;
-		} else {
-			cards[0].className = 'card';
-			cards[1].className = 'card';
-		}
-
-		cards.pop();
-		cards.pop();
-		canFlip = true;
-
-		if (cardPairs === 8)
-			displayWinMessage();
-	}, 400); // wait 0.4 seconds to check both cards
-
-	let moveCount = parseInt(moves.textContent);
-	moveCount++;
-	moves.textContent = moveCount.toString();
-
-	if (moveCount === 16) {
-		stars.childNodes[5].firstElementChild.className = 'far fa-star'; // makes stars white
-		starCount--;
-	} else if (moveCount === 26) {
-		stars.childNodes[3].firstElementChild.className = 'far fa-star';
-		starCount--;
-	}
-}
-
-function displayWinMessage() {
+const displayWinMessage = () => {
 	/* <div style="text-align: center; padding: 10% 0">
 	 * 		<h1 style="font-weight: bold">Congratulations! You Won!</h1>
 	 * 		<p style="white-space: pre">With x moves and x stars in x:xx.\r\nWoooooo!</p>
@@ -169,3 +123,49 @@ function displayWinMessage() {
 	}
 	winMessage.appendChild(playAgainButton);
 }
+
+const checkCards = (cards) => {
+	canFlip = false; // disable flipping cards while checking
+
+	setTimeout(function() {
+		if (cards[0].firstElementChild.className === cards[1].firstElementChild.className) {
+			cards[0].className = 'card match';
+			cards[1].className = 'card match';
+			cardPairs++;
+		} else {
+			cards[0].className = 'card';
+			cards[1].className = 'card';
+		}
+
+		cards.pop();
+		cards.pop();
+		canFlip = true;
+
+		if (cardPairs === 8)
+			displayWinMessage();
+	}, 400); // wait 0.4 seconds to check both cards
+
+	let moveCount = parseInt(moves.textContent);
+	moveCount++;
+	moves.textContent = moveCount.toString();
+
+	if (moveCount === 16) {
+		stars.childNodes[5].firstElementChild.className = 'far fa-star'; // makes stars white
+		starCount--;
+	} else if (moveCount === 26) {
+		stars.childNodes[3].firstElementChild.className = 'far fa-star';
+		starCount--;
+	}
+}
+
+deck.addEventListener('click', (event) => {
+	if (canFlip) {
+		const card = event.target;
+
+		if (card.className === 'card')
+			flipCard(card);
+
+		if (openCards.length === 2)
+			checkCards(openCards);
+	}
+});
